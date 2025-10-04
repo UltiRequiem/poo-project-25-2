4a3d68202a1c1ee24bf6f81aa3d6f2bd7aa7c79e
Leonardo Estacio <mdnssknght@gmail.com>
2025-10-04 16:03:35 -0500

    chore: change type of button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default async function Dashboard() {
             </div>
             <div className="flex items-center gap-3">
               <a href="/create-product">
-                <button className="flex items-center gap-2 px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition-colors" type="submit">
+                <button className="flex items-center gap-2 px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition-colors" type="button">
                   <svg
                     className="w-5 h-5"
                     fill="none"
