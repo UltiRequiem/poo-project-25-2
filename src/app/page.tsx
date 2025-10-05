6e470a0caae51dff45289dc2d69286f0e173769a
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2025-10-04 21:40:22 -0500

    chore: create footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,7 +134,6 @@ export default async function Dashboard() {
           </div>
         </div>
 
-        {/* Business Analysis Section */}
         <BusinessAnalysis products={serializedProducts} />
       </main>
     </div>
